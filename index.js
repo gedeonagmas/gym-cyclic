029d1e4b5f6f5dd5807b438d4b55771fdfa82b07
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-09 09:44:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("*", (req, res, next) => {
 
 mongodb()
   .then(() => {
-    app.listen(process.env.PORT, (err) => {
+    app.listen(3000, (err) => {
       if (err) console.log(err);
       console.log("gym server connected on port", process.env.PORT);
     });
